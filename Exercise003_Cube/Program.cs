ce48825a3c097fbdfa9703b2924ae07c207e55c1
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-18 13:53:55 +0300

    Done003

---
+++ Exercise003_Cube/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int n = Prompt("Enter a munber: ");
+
+for(int i = 1; i <= n; i++)
+{
+    Console.Write($" {Math.Pow(i,3)};");
+}
