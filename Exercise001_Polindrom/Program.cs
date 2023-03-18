474b2115d72598078faaac507548075a554f3da5
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-18 13:26:54 +0300

    Done001

---
+++ Exercise001_Polindrom/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int a = Prompt("Введите пятизначное число: ");
+
+if (a / 10000 == a % 10 && (a / 1000) % 10 == (a %100) / 10)
+{
+    Console.Write("Полиндром!");
+}
+else
+    Console.Write("Число не полиндром!");
